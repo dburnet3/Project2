8fcfaa65ad766d393ab4805a6b01845689a22b71
dburnet3 <deniseburnett26@gmail.com>
2019-07-08 19:27:02 -0500

    added account signup logic using firebase and discarded use of passport

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const htmlRoutes = require("./controllers/html-routes");
 var bodyParser = require('body-parser');
 var urlencode = bodyParser.urlencoded({ extended: true});
 
+//Passport Config
 app.use(express.static('public'));
 
 var apis = require('./controllers/api-routes');
@@ -16,9 +17,14 @@ app.use('/', apis);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 app.use(apiRoutes);
 app.use(htmlRoutes);
 app.use(express.static("public"));
+
+//Requiring our routes
+require("./controllers/api-routes.js");
+require("./controllers/html-routes.js");
 
 db.sequelize.sync({ force: false}).then(() => {
     app.listen(PORT,
