63203ca646ed0ec96e88e657f41e04afc7bf2428
dburnet3 <deniseburnett26@gmail.com>
2019-07-11 11:14:43 -0400

    wrong branch, I know...but I updated the login redirects, db schema that contains the school tuition, and updated routing paths

--- server.js
+++ server.js
@@ -4,14 +4,7 @@ const app = express();
 const db = require("./models");
 const apiRoutes = require("./controllers/api-routes");
 const htmlRoutes = require("./controllers/html-routes");
-var bodyParser = require('body-parser');
-var urlencode = bodyParser.urlencoded({ extended: true});
 
-//Passport Config
-app.use(express.static('public'));
-
-var apis = require('./controllers/api-routes');
-app.use('/', apis);
 
 //middleware
 app.use(express.urlencoded({ extended: true }));
@@ -22,13 +15,9 @@ app.use(apiRoutes);
 app.use(htmlRoutes);
 app.use(express.static("public"));
 
-//Requiring our routes
-require("./controllers/api-routes.js");
-require("./controllers/html-routes.js");
 
 db.sequelize.sync({ force: false}).then(() => {
     app.listen(PORT,
         () => console.log(`http://localhost:${PORT}`));
 });
 
-module.exports = app;
