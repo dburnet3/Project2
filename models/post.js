2e554ad61d4a585e7ff1041f58836a43c8ed5d8d
dburnet3 <deniseburnett26@gmail.com>
2019-07-16 14:10:06 -0400

    something

--- models/post.js
+++ models/post.js
@@ -1,10 +1,10 @@
-module.exports = function(sequelize, DataTypes) {
-    var Post = sequelize.define("Post", {
-        Ethnicity: DataTypes.TEXT,
-        Choices: DataTypes.TEXT,
-        Major: DataTypes.TEXT,
-        Cost: DataTypes.TEXT
-    });
-    return Post;
-  };
+// module.exports = function(sequelize, DataTypes) {
+//     var Post = sequelize.define("Post", {
+//         Ethnicity: DataTypes.TEXT,
+//         Choices: DataTypes.TEXT,
+//         Major: DataTypes.TEXT,
+//         Cost: DataTypes.TEXT
+//     });
+//     return Post;
+//   };
 
